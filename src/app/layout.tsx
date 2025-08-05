d9ba541fcedddbe1a80c5ef124a96ed7cf9c5891
Akash <aakashrajp5@gmail.com>
2025-08-05 15:03:40 +0530

    feat: Add Vercel Analytics for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from './providers';
 import { ReduxProvider } from './ReduxProvider';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ReduxProvider>
           <Providers>
             {children}
+            <Analytics />
           </Providers>
         </ReduxProvider>
       </body>
